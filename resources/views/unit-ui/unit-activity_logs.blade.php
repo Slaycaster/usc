3a275a9edf5a1f68adf110c5151e788a89b71f4f
Eugene Grava <eugenegrava.c@gmail.com>
2016-04-11 17:28:18 +0800

    Created new html for activity log
    this html is only a template
    no angularjs yet
    
    
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/unit-ui/unit-activity_logs.blade.php
+++ resources/views/unit-ui/unit-activity_logs.blade.php
@@ -2,6 +2,97 @@
 
 @section('content')
    
-    <h1>Hello</h1>
+    <div ng-app="unitScorecardApp" ng-controller="APIUnitActivityLogController">
+        <div class="wrap">
+            <div class="row">           
+                <div class="panel panel-warning">
+                    <div class="col-lg-12 unitdashboard-custom-unit-activity">
+                        <i class="fa fa-bell fa-2x"></i><p class="page-header"><b>{{ $user->unit->UnitName }} Activity Log</b></p>
+                    </div>
+
+                     <div class="col-lg-12 unitdashboard-custom-unit-activityabb">
+                        <i class="fa fa-bell fa-2x"></i><p class="page-header"><b>{{ $user->unit->UnitAbbreviation }} Activity Log</b></p>
+                    </div>
+
+                    <div class="panel-body">
+                        <div class="row">
+                            <!-- <div class="col-lg-4">
+                                <button id="btn-add" class="btn btn-primary btn-block btn-md" ng-click="toggle('add', 0)">Add New Unit's Measure</button>
+                            </div> -->
+                            <div class="col-lg-4 pull-right">
+                                <form>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon">
+                                                <i class="fa fa-search fa-fw"></i>
+                                            </span>
+                                            <input type="text" ng-model="search" class="form-control" placeholder="Search here...">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!--/.div class row-->
+                        <div class="row">
+                            <div class="alert alert-info"><i class="fa fa-info-circle fa-fw"></i> Click on the table's column in order to sort ascending or descending.</div>
+                        </div>
+                        <!--./div class row-->
+
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered">
+                                <thead>
+                                    <td ng-click=""><b>Action</b>
+                                        <span class="glyphicon sort-icon" ng-show="" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
+                                    </td>
+                            
+                                    <td ng-click=""><b>Last Encoded By</b>
+                                        <span class="glyphicon sort-icon" ng-show="" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
+                                    </td>
+
+                                    </td>
+                                    <td ng-click=""><b>Created at</b>
+                                        <span class="" ng-show="" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
+                                    </td>
+                                    <td ng-click=""><b>Updated by</b>
+                                        <span class="" ng-show="" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
+                                    </td>
+                                    <td></td>
+                                </thead>
+                                <!-- <tr dir-paginate='unit_measure in unit_measures|orderBy:sortKey:reverse|filter:search|itemsPerPage:5'>
+                                    <td><% unit_measure.UnitMeasureName %></td>
+                                    <td><% unit_measure.UnitMeasureType %></td>
+                                    <td><% unit_measure.unit.UnitAbbreviation %></td>
+                                    <td><% unit_measure.user_unit.rank.RankCode %> <% unit_measure.user_unit.UserUnitFirstName %> <% unit_measure.user_unit.UserUnitLastName %></td>
+                                </tr> -->
+                            </table>
+                        </div>
+                        <!--./table table striped-->
+                        <center>
+                            <dir-pagination-controls
+                               max-size="5"
+                               direction-links="true"
+                               boundary-links="true" >
+                            </dir-pagination-controls>
+                            <!--./dir-pagination-controls-->
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    <!-- Load Javascript Libraries (AngularJS, JQuery, Bootstrap) -->
+    <script src="{{ asset('bower_components/angular/angular.min.js') }}"></script>
+
+    <!-- Angular Utils Pagination -->
+    <script src="{{ asset('bower_components/angularUtils-pagination/dirPagination.js') }}"></script>
+  
+    <!-- AngularJS Application Scripts -->
+    <script src="{{ asset('app/app.js') }}"></script>
+    
+    <!-- AngularJS Application Scripts -->
+    <!-- does not exist yet -->
+    <script src="{{ asset('app/controllers/unit_activity_logs.js') }}"></script>
      
 @endsection
