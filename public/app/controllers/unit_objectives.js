a4a0cf042d9f34d3695627b3ac974ee08e7e201c
Eugene Grava <eugenegrava.c@gmail.com>
2016-04-12 13:32:15 +0800

    Edited unit_objectives
    removed commented lines from app.js
    
    
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- public/app/controllers/unit_objectives.js
+++ public/app/controllers/unit_objectives.js
@@ -1,13 +1,3 @@
-// console.log('response');
-// app.controller('unitObjectivesController', function($scope, $http) {
-//     //retrieve Unit Objectives listing from API
-//     $http.get("http://localhost/usc/public/api/unit_objectives")
-//             .success(function(response) {
-//                 $scope.unit_objectives = response;
-//             });
-    
-    
-// });
 
  
 app.controller('unitObjectiveController', function($scope, $http) {
