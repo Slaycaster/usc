2367081d1d19ed3d6170233f3f8a7871b80b65b4
Eugene Grava <eugenegrava.c@gmail.com>
2016-04-12 13:33:27 +0800

    Edited unit_measures.js for spaces
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- public/app/controllers/unit_measures.js
+++ public/app/controllers/unit_measures.js
@@ -6,19 +6,17 @@ app.controller('APIUnitMeasureController', function($scope, $http) {
 	$scope.loading = false;
  
 	$scope.init = function() 
-	{
+    {
 		$scope.loading = true;
 		$http.get(local + '/usc/public/api/unit_measures').
 		success(function(data, status, headers, config) {
 			$scope.unit_measures = data;
 				$scope.loading = false;
-		});
-
-		
+		});	
 	};
 
 	$scope.sort = function(keyname)
-	{
+    {
         $scope.sortKey = keyname;   //set the sortKey to the param passed
         $scope.reverse = !$scope.reverse; //if true make it false and vice versa
     };
