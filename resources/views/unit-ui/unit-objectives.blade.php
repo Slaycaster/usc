dd17b3d936524483098bbba83d63a2ef3e617df1
Eugene Grava <eugenegrava.c@gmail.com>
2016-04-12 15:49:29 +0800

    Transferred the script from bottom to top
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/unit-ui/unit-objectives.blade.php
+++ resources/views/unit-ui/unit-objectives.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
+    <!-- Load Javascript Libraries (AngularJS, JQuery, Bootstrap) -->
+    <script src="{{ asset('bower_components/angular/angular.min.js') }}"></script>
+
+    <!-- Angular Utils Pagination -->
+    <script src="{{ asset('bower_components/angularUtils-pagination/dirPagination.js') }}"></script>
+
+    <!-- AngularJS Application Scripts -->
+    <script src="{{ asset('app/app.js') }}"></script>
+
+    <!-- AngularJS Application Scripts -->
+    <script src="{{ asset('app/controllers/unit_objectives.js') }}"></script>
+
     <br>
 	<div ng-app="unitScorecardApp" ng-controller="unitObjectiveController">
 	    <div class="wrap">
@@ -35,21 +47,21 @@
 							</div>
 							<!--./div class row-->
                             <div class="table-responsive">
-    							<table class="table table-striped  table-bordered">
+    							<table class="table table-striped table-bordered">
     								<thead>
-    									<td ng-click="sort('unit_objective.UnitObjectiveName')"><b>Unit Objective Name</b>
+    									<td class="objective-custom-td1" ng-click="sort('unit_objective.UnitObjectiveName')"><b>Unit Objective Name</b>
     										<span class="glyphicon sort-icon" ng-show="sortKey=='unit_objective.UnitObjectiveName'" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
     									</td>
-    									<td ng-click="sort('unit_objective.perspective.PerspectiveName')"><b>Perspective</b>
+    									<td class="objective-custom-td2" ng-click="sort('unit_objective.perspective.PerspectiveName')"><b>Perspective</b>
     										<span class="glyphicon sort-icon" ng-show="sortKey=='unit_objective.perspective.PerspectiveName'" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
     									</td>
-    									<td ng-click="sort('unit_objective.unit.UnitAbbreviation')"><b>Unit</b>
+    									<td class="objective-custom-td3" ng-click="sort('unit_objective.unit.UnitAbbreviation')"><b>Unit</b>
     										<span class="glyphicon sort-icon" ng-show="sortKey=='unit_objective.unit.UnitAbbreviation'" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
     									</td>
-    									<td ng-click="sort('unit_objective.user_unit.rank.RankCode')"><b>Last Encoded by</b>
+    									<td class="objective-custom-td4" ng-click="sort('unit_objective.user_unit.rank.RankCode')"><b>Last Encoded by</b>
     										<span class="glyphicon sort-icon" ng-show="sortKey=='unit_objective.user_unit.rank.RankCode'" ng-class="{'glyphicon-chevron-up':reverse,'glyphicon-chevron-down':!reverse}"></span>
     									</td>
-    									<td></td>
+    									<td class="objective-custom-td5"></td>
     								</thead>
     								<tr dir-paginate='unit_objective in unit_objectives|orderBy:sortKey:reverse|filter:search|itemsPerPage:5'>
     									<td><% unit_objective.UnitObjectiveName %></td>
@@ -87,10 +99,56 @@
                         <i class="fa fa-circle-o-notch fa-4x"></i>
                         <h4 class="modal-title" id="myModalLabel"><b><% form_title %></b></h4>
                     </div>
+
                     <div class="modal-body">
                         <form name="frmEditObjective" class="form-horizontal" novalidate="">
+                            <table class="table table-responsive">
+                                <tr>
+                                    <td>
+                                        <label for="objective_name" class="control-label">Objective Name:</label>
+                                    </td>
+                                    <td>
+                                        <input type='text' name="objective_name" value="<% unit_objective.UnitObjectiveName %>" ng-model="unit_objective.UnitObjectiveName" autocomplete="off" class="form-control" required ng-touched>
+                                        <span class="help-inline" ng-show="userForm.objective_name.$invalid && !userForm.objective_name.$pristine">Objective Name is required.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label for="perspective_id" class="control-label">Perspective:</label>
+                                    </td>
+                                    <td>
+                                        <select name="perspective_id" data-ng-model="unit_objective.PerspectiveID" class="form-control" required ng-touched>
+                                            @foreach($perspectives as $perspective)
+                                                    <option value="<?=$perspective->PerspectiveID?>">{{ $perspective->PerspectiveName }}</option>
+                                            @endforeach
+                                        </select>
+                                        <span ng-show="userForm.perspective_id.$invalid && !userForm.perspective_id.$pristine" class="help-inline">Perspective is required.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label for="inputEmail3" class="control-label">Unit:</label>
+                                    </td>
+                                    <td>
+                                        <p>{{ $user->unit->UnitName }}</p>
+                                        <input type="hidden" name="UnitID" value="<?=$user->unit->UnitID?>" id="unit_id">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label for="inputEmail3" class="control-label">Account User:</label>
+                                    </td>
+                                    <td>
+                                        <p>{{ $user->rank->RankCode }} {{ $user->UserUnitFirstName }} {{ $user->UserUnitLastName }} </p>
+                                        <input type="hidden" name="UserUnitID" value="<?=$user->UserUnitID?>" id="user_unit_id">
+                                    </td>
+                                </tr>
+                            </table>
 
-                            <div class="form-group error">
+
+
+
+                            <!-- <div class="form-group error">
                                 <label for="objective_name" class="col-sm-3 control-label">Objective Name</label>
                                 <div class="col-sm-9">
                                     <input type='text' name="objective_name" value="<% unit_objective.UnitObjectiveName %>" ng-model="unit_objective.UnitObjectiveName" autocomplete="off" class="form-control" required ng-touched>
@@ -119,11 +177,11 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="inputEmail3" class="col-sm-3 control-label">Last Encoded by:</label>
+                                <label for="inputEmail3" class="col-sm-3 control-label">Account User:</label>
                                 <div class="col-sm-9">
                                     <p>{{ $user->rank->RankCode }} {{ $user->UserUnitFirstName }} {{ $user->UserUnitLastName }} </p>
                                     <input type="hidden" name="UserUnitID" value="<?=$user->UserUnitID?>" id="user_unit_id">
-                            </div>
+                            </div> -->
 
                         </form>
                     </div>
@@ -134,18 +192,6 @@
             </div>
         </div>
 	</div>
-
-    <!-- Load Javascript Libraries (AngularJS, JQuery, Bootstrap) -->
-    <script src="{{ asset('bower_components/angular/angular.min.js') }}"></script>
-
-    <!-- Angular Utils Pagination -->
-    <script src="{{ asset('bower_components/angularUtils-pagination/dirPagination.js') }}"></script>
-
-    <!-- AngularJS Application Scripts -->
-    <script src="{{ asset('app/app.js') }}"></script>
-
-    <!-- AngularJS Application Scripts -->
-    <script src="{{ asset('app/controllers/unit_objectives.js') }}"></script>
   
 
 @endsection
