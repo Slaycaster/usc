3596fefc87794ea2407a3167838641abe3b2aff5
Eugene Grava <eugenegrava.c@gmail.com>
2016-04-12 13:35:29 +0800

    Added controller for audit trails
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

---
+++ app/Http/Controllers/APIUnitAuditTrailsController.php
@@ -0,0 +1,50 @@
+<?php namespace App\Http\Controllers;
+
+use App\AuditTrail;
+use App\UserUnit;
+use App\Unit;
+use App\Http\Controllers\Controller;
+use Request, Session;
+
+
+
+class APIUnitAuditTrailsController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+        $id = Session::get('unit_user_id', 'default');
+        $unit = UserUnit::where('UserUnitID', '=', $id)->select('UnitID')->lists('UnitID'); //Get the Unit of the user
+        
+        return AuditTrail::where('UnitID', '=', $unit)
+            ->with('user_unit')
+            ->with('user_unit.rank')
+            ->get();
+    }
+
+    public function showIndex()
+    {
+        if (Session::has('unit_user_id'))
+        {
+            $id = Session::get('unit_user_id', 'default');
+            $user = UserUnit::where('UserUnitID', $id)
+                ->first();
+            $unit_audit_trails = AuditTrail::where('UserUnitID', '=', $user->UnitID)->get();
+            
+            return view('unit-ui.unit-audit_trails')
+                ->with('user', $user)
+                ->with('unit_audit_trails', $unit_audit_trails);
+        }
+        else
+        {
+            Session::flash('message', 'Please login first!');
+            return Redirect::to('/');
+        }
+    }
+
+}
