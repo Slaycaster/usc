8114a6ff4fe381587884a59819a166d18dc35b56
Eugene Grava <eugenegrava.c@gmail.com>
2016-04-28 13:17:05 +0800

    Added routes for chief
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,23 +19,24 @@ Route::get('home', 'HomeController@index');
 Route::post('login', 'LoginController@doLogin');
 Route::get('logout', 'LoginController@doLogout');
 
-/*UNIT USER ROUTES*/
+/*DASHBOARD*/
+Route::get('chief/dashboard', 'ChiefLoginController@dashboard');
 Route::get('unit/dashboard', 'UnitLoginController@dashboard');
+Route::get('staff/dashboard', 'StaffLoginController@dashboard');
+
+/*UNIT USER ROUTES*/
 Route::get('unit/objectives', 'APIUnitObjectivesController@showIndex');
 Route::get('unit/setscorecard', 'UnitSetScorecardController@index');
 Route::get('unit/scorecard', 'UnitScorecardController@showIndex');
 Route::get('unit/measures','APIUnitMeasuresController@showIndex');
 Route::get('unit/audit_trails', 'APIUnitAuditTrailsController@showIndex');
-Route::get('unit/audit_trails/api', 'APIUnitAuditTrailsController@api');
 
 /*CHIEF USER ROUTES*/
 
 /*STAFF USER ROUTES*/
 
-/*DASHBOARD*/
-Route::get('chief/dashboard', 'ChiefLoginController@dashboard');
+/* DASHBOARD APPLETS */
 Route::get('unit/unitdashboard', 'APIUnitAuditTrailsDashController@showIndex');
-Route::get('staff/dashboard', 'StaffLoginController@dashboard');
 
 /*API ROUTES*/
 Route::resource('api/unit_objectives','APIUnitObjectivesController');
