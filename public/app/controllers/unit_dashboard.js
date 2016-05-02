68e5ffb0561fc0ed88c7e99be7cf5d1ade9540a3
Eugene Grava <eugenegrava.c@gmail.com>
2016-05-02 10:47:09 +0800

    Get the location.host
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- public/app/controllers/unit_dashboard.js
+++ public/app/controllers/unit_dashboard.js
@@ -1,4 +1,4 @@
-var local = 'http://localhost';
+var local = 'http://' + location.host;
 
 app.controller('APIUnitAuditTrailsDashController', function($scope, $http, $interval) {
     $scope.unit_audit_trails_dash = [];
