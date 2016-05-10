0a02011c4266b7efb1394594cd96059bc3443eb8
Slaycaster-Toshiba <fdenimar@hotmail.com>
2016-05-10 14:39:49 +0800

    Modified route
    Signed-off-by: Slaycaster-Toshiba <fdenimar@hotmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,6 @@
 */
 
 Route::get('/', 'WelcomeController@index');
-
 Route::get('home', 'HomeController@index');
 
 /*LOGIN ROUTE*/
@@ -20,24 +19,18 @@ Route::post('login', 'LoginController@doLogin');
 Route::get('logout', 'LoginController@doLogout');
 
 /*DASHBOARD*/
-Route::get('chief/dashboard', 'ChiefLoginController@dashboard');
 Route::get('unit/dashboard', 'UnitLoginController@dashboard');
+Route::get('unit/unitdashboard', 'APIUnitAuditTrailsDashController@showIndex');
 Route::get('staff/dashboard', 'StaffLoginController@dashboard');
+Route::get('chief/dashboard', 'ChiefLoginController@dashboard');
+
 
 /*UNIT USER ROUTES*/
 Route::get('unit/objectives', 'APIUnitObjectivesController@showIndex');
-Route::get('unit/setscorecard', 'UnitSetScorecardController@index');
-Route::get('unit/scorecard', 'UnitScorecardController@index');
 Route::get('unit/measures','APIUnitMeasuresController@showIndex');
 Route::get('unit/audit_trails', 'APIUnitAuditTrailsController@showIndex');
-
-
-
-/*CHIEF USER ROUTES*/
-Route::get('chief/objectives', 'APIChiefObjectivesController@showIndex');
-Route::get('chief/measures','APIChiefMeasuresController@showIndex');
-Route::get('chief/targets','APIChiefTargetsController@showIndex');
-Route::get('chief/targets/{id}','APIChiefTargetsController@edit');
+Route::get('unit/setscorecard', 'UnitSetScorecardController@index');
+Route::get('unit/scorecard', 'UnitScorecardController@index');
 
 
 /*STAFF USER ROUTES*/
@@ -45,8 +38,12 @@ Route::get('staff/dashboard', 'StaffLoginController@dashboard');
 Route::get('staff/objectives', 'APIStaffObjectivesController@showIndex');
 Route::get('staff/measures','APIStaffMeasuresController@showIndex');
 
-/* DASHBOARD APPLETS */
-Route::get('unit/unitdashboard', 'APIUnitAuditTrailsDashController@showIndex');
+
+/*CHIEF USER ROUTES*/
+Route::get('chief/objectives', 'APIChiefObjectivesController@showIndex');
+Route::get('chief/measures','APIChiefMeasuresController@showIndex');
+Route::get('chief/targets','APIChiefTargetsController@showIndex');
+Route::get('chief/targets/{id}','APIChiefTargetsController@edit');
 
 
 /*API ROUTES*/
@@ -59,11 +56,8 @@ Route::resource('api/chief_objectives', 'APIChiefObjectivesController');
 
 
 Route::get('api/perspectives', 'PerspectiveController@allPerspectives');
-
 Route::get('api/staff/objectives/chiefobjectives', 'APIStaffObjectivesController@chief_objectives');
 Route::get('api/unit/objectives/staffobjectives', 'APIUnitObjectivesController@staff_objectives');
-
-
 
 
 Route::resource('api/chief_measures','APIChiefMeasuresController');
