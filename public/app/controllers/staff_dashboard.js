0bfd1a8d13958e287a6a2e91ec4762578e65ff82
Lorenzo Joshua Viovicente <lorenzoviovicente@gmail.com>
2016-05-12 10:13:49 +0800

    Added new js for audit trail in dashboard
    Signed-off-by: Lorenzo Joshua Viovicente <lorenzoviovicente@gmail.com>

---
+++ public/app/controllers/staff_dashboard.js
@@ -0,0 +1,23 @@
+var local = 'http://' + location.host;
+
+app.controller('APIStaffAuditTrailsDashController', function($scope, $http, $interval) {
+    $scope.staff_audit_trails_dash = [];
+    $scope.loading = true;
+
+    $scope.init = function() {
+        $scope.loading = false;
+        $http.get(local + '/usc/public/api/staff_dashboard').
+        success(function(data, status, headers, config) {
+            nowTime = (new Date()).getTime();
+            $scope.staff_audit_trails_dash = data;
+                $scope.loading = false;
+        }); 
+    };
+
+    $scope.sort = function(keyname){
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    };
+    $interval( function(){ $scope.init(); }, 5000);
+    
+});
