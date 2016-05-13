360bb9ea19ab19db1ef5259496d6c75694cc76f6
Lorenzo Joshua Viovicente <lorenzoviovicente@gmail.com>
2016-05-13 13:28:14 +0800

    Modified files for unit user UI
    Signed-off-by: Lorenzo Joshua Viovicente <lorenzoviovicente@gmail.com>

--- resources/views/layout-unit.blade.php
+++ resources/views/layout-unit.blade.php
@@ -68,6 +68,7 @@
     <div id="wrapper">
 
         <!-- Navigation -->
+        
         <nav class="layout-title-navbar navbar navbar-default navbar-fixed-top" role="navigation">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -110,8 +111,9 @@
                 <!-- /.dropdown -->
             </ul>
             <!-- /.navbar-top-links -->
-            
+        </nav>
 
+        <nav class="layout-title-navbar navbar navbar-default side-nav" role="navigation">
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
@@ -201,13 +203,16 @@
                             </ul>
                             
                         </li>
-                    	-->
+                        -->
                     </ul>
                 </div>
                 <!-- /.sidebar-collapse -->
             </div>
             <!-- /.navbar-static-side -->
         </nav>
+
+
+        
         <div class="the-blur"></div>
 
         <br><br>
