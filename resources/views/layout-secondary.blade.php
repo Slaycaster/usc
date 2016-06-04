3dd89ddac436ed9c3133dc1248d6ec81fb7ea80e
Eugene Grava <eugenegrava.c@gmail.com>
2016-06-04 15:24:27 +0800

    Link for secondaryunit/targets
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/layout-secondary.blade.php
+++ resources/views/layout-secondary.blade.php
@@ -180,7 +180,7 @@
                             <!-- /.nav-second-level -->
                         </li>
                         <li>
-                            <a href="{{ url('secondary/targets') }}"><i class="fa fa-table fa-fw"></i> Set Measure Targets</a>
+                            <a href="{{ url('secondaryunit/targets') }}"><i class="fa fa-table fa-fw"></i> Set Measure Targets</a>
                         </li>
                         <li>
                             <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Queries/Reports<span class="fa arrow"></span></a>
