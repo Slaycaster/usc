f1096e253e9090731d32c8da27931b200140e01b
Lorenzo Joshua Viovicente <lorenzoviovicente@gmail.com>
2016-06-06 09:43:03 +0800

    Updated Secondary Unit Models
    Signed-off-by: Lorenzo Joshua Viovicente <lorenzoviovicente@gmail.com>

--- app/SecondaryUnitInitiative.php
+++ app/SecondaryUnitInitiative.php
@@ -18,4 +18,25 @@ class SecondaryUnitInitiative extends Model {
 	 */
 	protected $primaryKey = 'SecondaryUnitInitiativeID';
 
+
+	public function secondary_unit()
+	{
+		return $this->belongsTo('App\SecondartUnit', 'SecondaryUnitID', 'SecondaryUnitID');
+	}
+
+	public function user_secondary_unit()
+	{
+		return $this->belongsTo('App\UserSecondaryUnit','UserSecondaryUnitID','UserSecondaryUnitID'); //(model, foreign_key, parent_primary_key)
+	}
+
+	public function secondary_unit_measure()
+	{
+		return $this->belongsTo('App\SecondaryUnitMeasure', 'SecondaryUnitMeasureID', 'SecondaryUnitMeasureID');
+	}
+
+	public function secondary_unit_target()
+	{
+		return $this->hasOne('App\SecondaryUnitTarget', 'SecondaryUnitTargetID', 'SecondaryUnitTargetID'); //(model, foreign_key, parent_primary_key)
+	}
+
 }
