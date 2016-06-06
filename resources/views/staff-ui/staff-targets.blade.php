81cbd3085a5f5f96e625616240f3e098aecc1877
Eugene Grava <eugenegrava.c@gmail.com>
2016-06-06 16:49:38 +0800

    Added ng-model for monthly target
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/staff-ui/staff-targets.blade.php
+++ resources/views/staff-ui/staff-targets.blade.php
@@ -336,7 +336,7 @@
             </div>
         </div>
 
-                <div class="modal fade" id="alreadysetModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal fade" id="alreadysetModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog ">
                 <div class="modal-content">
                     <div class="modal-header">
