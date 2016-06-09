2177aa9117a23e1540d91c40c05cd437579e53f5
Eugene Grava <eugenegrava.c@gmail.com>
2016-06-09 14:22:12 +0800

    Modified for tableinfo
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/chief-ui/chief-measures.blade.php
+++ resources/views/chief-ui/chief-measures.blade.php
@@ -44,7 +44,7 @@
 								</div>
 							</div>
 							<!--/.div class row-->
-							<div class="row">
+							<div class="row" id="tableinfo">
                                 <div ng-show="info" class="alert alert-info objective-info-name"><i class="fa fa-info-circle fa-fw"></i>Chief Measures of {{ $chief_user->chief->ChiefName }}.</div>
                                 <div ng-show="info" class="alert alert-info objective-info-abb"><i class="fa fa-info-circle fa-fw"></i>Chief Measures of {{ $chief_user->chief->ChiefAbbreviation }}.</div>
                             </div>
--- resources/views/tertiary-ui/tertiary-objectives.blade.php
+++ resources/views/tertiary-ui/tertiary-objectives.blade.php
@@ -49,7 +49,7 @@
                                 </div>
                             </div>
                             <!--/.div class row-->
-                            <div class="row">
+                            <div class="row" id="tableinfo">
                                 <div ng-show="info" class="alert alert-info objective-info-name"><i class="fa fa-info-circle fa-fw"></i> Tertiary Unit Objectives of {{ $user->tertiary_unit->TertiaryUnitName }}.
                                 </div>
                                 <div ng-show="info" class="alert alert-info objective-info-abb"><i class="fa fa-info-circle fa-fw"></i> Tertiary Unit Objectives of {{ $user->tertiary_unit->TertiaryUnitAbbreviation }}.
