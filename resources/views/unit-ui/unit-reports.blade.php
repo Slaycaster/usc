ba9f20174c1b38c2a21a8c757ed7764eb3a45084
Eugene Grava <eugenegrava.c@gmail.com>
2016-06-09 14:53:56 +0800

    Removed excess comma
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/unit-ui/unit-reports.blade.php
+++ resources/views/unit-ui/unit-reports.blade.php
@@ -5,11 +5,11 @@
  <!-- Load Javascript Libraries (AngularJS, JQuery, Bootstrap) -->
     <script src="{{ asset('bower_components/angular/angular.min.js') }}"></script>
 
-    <!-- Angular Utils Pagination -->
-    <script src="{{ asset('bower_components/angularUtils-pagination/dirPagination.js') }}"></script>
-  
     <!-- AngularJS Application Scripts -->
     <script src="{{ asset('app/app.js') }}"></script>
+
+    <!-- Angular Utils Pagination -->
+    <script src="{{ asset('bower_components/angularUtils-pagination/dirPagination.js') }}"></script>
     
     <!-- AngularJS Application Scripts -->
     <script src="{{ asset('app/controllers/unit_targets.js') }}"></script>
