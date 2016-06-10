c9fdb77519182acd9719c6a16d8154f2df3a0c19
Eugene Grava <eugenegrava.c@gmail.com>
2016-06-10 09:45:30 +0800

    Set the reload interval to 120
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="60">
+    <meta http-equiv="refresh" content="120">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#363636 ">
     <meta name="msapplication-navbutton-color" content="#363636 ">
