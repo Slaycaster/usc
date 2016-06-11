2617dfd1db013ef1b894b573eb51edbb168a5205
Eugene Grava <eugenegrava.c@gmail.com>
2016-06-11 22:00:26 +0800

    Added ng-show
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/chiefdashboard.blade.php
+++ resources/views/chiefdashboard.blade.php
@@ -189,7 +189,7 @@
                     <center><i ng-show="loading" class="fa fa-spinner fa-spin"></i></center>
                 </div>
 
-                <div class="list-group" dir-paginate='audit_trail_dash in chief_audit_trails_dash|orderBy:"updated_at":true:sortKey:reverse|itemsPerPage:5'>
+                <div class="list-group" dir-paginate='audit_trail_dash in chief_audit_trails_dash|orderBy:"updated_at":true:sortKey:reverse|itemsPerPage:5' ng-show="info">
                     <!-- <a href="{{ url('staff/audit_trails') }}" class="list-group-item"> -->
                     <a href="{{ url('chief/audit_trails') }}" class="list-group-item" style="font-size:12px;">
                     <span class="pull-right"><img ng-src="../uploads/userpictures/unit/cropped/<%audit_trail_dash.user_chief.UserChiefPicturePath%>" height="30px;">
